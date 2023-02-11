3ff16f5ad3673bc9e25943452bae4177e3b55bb0
DEVM0TECNM <124701514+DEVM0TECNM@users.noreply.github.com>
2023-02-11 11:24:06 -0600

    Add files via upload

---
+++ EVA1_1_HOLAMUNDO/src/main/java/tec2/eva1_1_holamundo/EVA1_1_HOLAMUNDO.java
@@ -0,0 +1,11 @@
+package tec2.eva1_1_holamundo;
+
+
+public class EVA1_1_HOLAMUNDO {
+//Alumno: Diego enrique vazquez montanez
+//materia: Programacion orientada a objetos
+//Docente: Hernandez chavez Ruben alonso
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!!");
+    }
+}
