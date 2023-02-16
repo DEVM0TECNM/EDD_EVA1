eb51062d0bd7e03db015e55e200155fb5ea709b8
DEVM0TECNM <124701514+DEVM0TECNM@users.noreply.github.com>
2023-02-16 17:33:47 -0600

    Add files via upload

---
+++ EVA1_5_CIRCULO/src/main/java/com/mycompany/eva1_5_circulo/EVA1_5_CIRCULO.java
@@ -0,0 +1,12 @@
+package com.mycompany.eva1_5_circulo;
+
+public class EVA1_5_CIRCULO {
+
+    public static void main(String[] args){
+        circulo circ = new circulo();     
+        circ.setRadio(13);
+        circ.setArea(0);
+        circ.setPerimetro(0);
+        circ.imprimirDatos();
+    }        
+}
---
+++ EVA1_5_CIRCULO/src/main/java/com/mycompany/eva1_5_circulo/circulo.java
@@ -0,0 +1,37 @@
+package com.mycompany.eva1_5_circulo;
+
+public class circulo {
+    private double perimetro;
+    private double area;
+    private double radio;
+        
+    public Circulo(){
+        radio = 0;
+        area = 0;
+        perimetro = 0;
+}
+public double getRadio(){
+        return radio;
+    }
+    public void setRadio(double valor){
+        radio = valor;
+    }
+    public double getPerimetro(){
+        return perimetro;
+    }
+    public void setPerimetro(double valor){
+        perimetro = 2 * Math.PI * radio;
+    }
+    public double getArea(){
+        return area;
+    }
+    public void setArea(double valor){
+        area = Math.PI * Math.pow(radio, 2);
+    }
+    public void imprimirDatos(){
+        System.out.println("Datos del circulo:");
+        System.out.println("Radio: " + radio);
+        System.out.println("Perimetro: " + perimetro);
+        System.out.println("Area: " + area);
+    }
+}
