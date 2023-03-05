251959baf8efc7fcf00800128e5b461297a00549
DEVM0TECNM <124701514+DEVM0TECNM@users.noreply.github.com>
2023-03-05 12:03:25 -0600

    Add files via upload

---
+++ EVA1_13_Alcanzar/src/main/java/com/mycompany/eva1_13_alcanzar/EVA1_13_Alcanzar.java
@@ -0,0 +1,22 @@
+package com.mycompany.eva1_13_alcanzar;
+
+public class EVA1_13_Alcanzar {
+
+    public static void main(String[] args) {
+        int x = 100;
+        for (int i = 0; i < 10; i++) {
+            System.out.println("x = " +x);
+            System.out.println("i= "+i);
+            int j = 100;
+            System.out.println("j = "+ j);
+            {
+              int z = 1000;
+                System.out.println(+z);
+            }
+        }       
+    }
+    public static void algo(){
+        
+        
+    }
+}
