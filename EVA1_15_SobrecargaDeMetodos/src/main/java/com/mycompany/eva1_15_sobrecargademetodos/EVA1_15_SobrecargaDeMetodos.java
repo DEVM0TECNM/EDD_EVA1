3a34f5056f32332ba60bfa39fe3f74082fbcf573
DEVM0TECNM <124701514+DEVM0TECNM@users.noreply.github.com>
2023-03-05 12:04:59 -0600

    Add files via upload

---
+++ EVA1_15_SobrecargaDeMetodos/src/main/java/com/mycompany/eva1_15_sobrecargademetodos/EVA1_15_SobrecargaDeMetodos.java
@@ -0,0 +1,11 @@
+package com.mycompany.eva1_15_sobrecargademetodos;
+
+public class EVA1_15_SobrecargaDeMetodos {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println(100);
+        System.out.println("true");
+        System.out.println(args);
+    }
+}
