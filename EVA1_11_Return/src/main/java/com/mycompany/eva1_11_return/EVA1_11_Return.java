788e6efd1cf7ef100b05af4da6615bd794b97cb9
DEVM0TECNM <124701514+DEVM0TECNM@users.noreply.github.com>
2023-03-05 12:02:15 -0600

    Add files via upload

---
+++ EVA1_11_Return/src/main/java/com/mycompany/eva1_11_return/EVA1_11_Return.java
@@ -0,0 +1,13 @@
+package com.mycompany.eva1_11_return;
+
+public class EVA1_11_Return {
+
+    public static void main(String[] args) {
+        int resu;
+        resu=square(10);
+        System.out.println("Resultado; "+resu);
+    }
+    public static int square(int num){
+        return num*num;
+    }
+    }
