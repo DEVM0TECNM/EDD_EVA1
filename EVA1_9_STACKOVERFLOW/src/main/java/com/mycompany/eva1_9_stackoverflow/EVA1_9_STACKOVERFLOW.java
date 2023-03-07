9d52dd0fa5762035ab1b21e7f68ddeebb9a1cf97
DEVM0TECNM <124701514+DEVM0TECNM@users.noreply.github.com>
2023-03-07 11:54:17 -0600

    Add files via upload

---
+++ EVA1_9_STACKOVERFLOW/src/main/java/com/mycompany/eva1_9_stackoverflow/EVA1_9_STACKOVERFLOW.java
@@ -0,0 +1,11 @@
+package com.mycompany.eva1_9_stackoverflow;
+
+public class EVA1_9_STACKOVERFLOW {
+
+    public static void main(String[] args) {
+        A();
+    }
+    public static void A(){
+        A();
+    }
+}
