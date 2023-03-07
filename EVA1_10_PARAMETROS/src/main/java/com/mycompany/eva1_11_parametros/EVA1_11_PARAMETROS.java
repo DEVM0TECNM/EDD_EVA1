b093715d48f7c03f1e53883de363817841161b10
DEVM0TECNM <124701514+DEVM0TECNM@users.noreply.github.com>
2023-03-07 11:55:43 -0600

    Add files via upload

---
+++ EVA1_10_PARAMETROS/src/main/java/com/mycompany/eva1_11_parametros/EVA1_11_PARAMETROS.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_11_parametros;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_11_PARAMETROS {
+
+    public static void main(String[] args) {
+     nPrintln("Hola", 5); 
+    }
+    public static void nPrintln(String message, int n){
+        for (int i = 0; i < n; i++) {
+            System.out.println(message);    
+        }
+}
+}
+    
